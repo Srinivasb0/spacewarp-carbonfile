1e3ebb7ac89400c305285e6008e22ebdc2a44504
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-01-31 21:45:36 +0530

    socML.ipynb file upload

---
+++ Scripts/socML.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+
+socDB = pd.read_csv('../Data/SOC_Database.csv', sep=',')
+
+# checking the data
+print(socDB.columns)
+print(socDB.shape)
+print(socDB.isnull().sum().any())
+print(socDB.duplicated().any())
+print(socDB.dtypes == 'str')
+
+# prepare data for ML modeling
+x = socDB.drop(['SampleID','SOC (%)'], 1)
+y = socDB['SOC (%)']
+
+
+
+
+
+

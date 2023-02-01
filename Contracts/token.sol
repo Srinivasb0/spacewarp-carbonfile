5ed9630d41ae85c19148c3c5af91895723700c13
Srinivas Borigarla <iamborigarla@Srinivass-MacBook-Pro.local>
2023-02-01 21:10:08 +0530

    updated token.sol push

--- Contracts/token.sol
+++ Contracts/token.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+// address : 0x0A8Cf146BDBA5CeA6784E7b68A9B7Ae82065F098s
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -9,12 +11,18 @@ contract CarbonFileToken is ERC20, Ownable {
     constructor() ERC20("Carbon File token", "CFTK") {}
     uint256 public tokenCount = 0;
     uint256 public maxSupply = 10;
-    uint256 amount = 1; // amount in Math.pow(10, 18)
+    uint256 amount = 1000000000000000000; // amount in Math.pow(10, 18)
 
-
+    struct tHolder {
+    address addr;
+    uint amount;
+    }
+    
+    // tHolder[] storage private dao;
     function mint() public payable {
         require(tokenCount <= maxSupply, "token limit reached");
         _mint(msg.sender, amount);
+        // dao.push({addr : msg.sender, amount : amount});
         tokenCount += 1;
     }
 
